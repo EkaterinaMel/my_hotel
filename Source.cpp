5006ceaec51d8b21eb76dc3cf5b0f2fd9c88868e
Katya <90666567+EkaterinaMel@users.noreply.github.com>
2022-04-21 01:26:12 +1000

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,230 @@
+#include <iostream> 
+#include <string>
+#include <list>
+#include "list.h"
+
+using namespace std;
+
+// 
+
+// Гостиница.
+// Сущности: дата, персона, ВИП-персона, комната, бронь, работник.
+//  
+// нужно сделать: 
+// 3: Контейнер + итератор. Отдельный хедер
+// 4: Внедрить стратегию или хотя бы команду
+// 
+// ?добавить?:
+// 
+// - разные работники
+// - расчёт зарплаты
+// - расчёт счетов за различные услуги, в сумме за всё пребывание
+// - доп. услуги
+// - таймер брони
+// - очередь на бронирование
+// - событие в номере
+// 
+//
+
+
+// дата 
+class date { 
+private:
+		int day; // день
+		int month; // месяц 
+		int year; // год
+public: 
+    date(int d, int m, int y) {
+		if ((d > 0)&&(d<32))
+			day = d;
+		else {
+			cout << "incorrect day";
+			day = 1;
+		}
+		if ((m>0)&&(m<13))
+			month = m;
+		else {
+			cout << "incorrect month";
+				month = 1;
+		}
+		if ((y>1800)&&(y<2200))
+			year = y;
+		else {
+			cout << "incorrect year";
+				year = 1111;
+		}
+	}
+
+	// перегрузка оператора присваивания
+	date &operator= (const date d)
+	{
+		day = d.day;
+		month = d.month;
+		year = d.year;
+
+		return *this;
+	}
+
+	// конструктор копирования
+	date(const date& d) {
+		day = d.day;
+		month = d.month;
+		year = d.year;
+	}
+
+
+	// печать
+	void print() {
+		if (day < 10) cout << "0";
+		cout << day << ".";
+		if (month < 10) cout << "0";
+		cout << month << "." << year << "\n";
+	}
+
+	};
+
+// сведения о персоне
+class person {
+private: 
+	string FIO; // ФИО
+	date birth = date( 1, 1, 2001 ); // дата рождения
+public:
+	void setPerson(string fio, date b) {
+		FIO = fio;
+		birth = b;
+	}
+	void print() {
+		cout << FIO << " ";
+		birth.print();
+	}
+	template <class T>
+	friend class employee; // дружественный шаблон работник
+};
+
+//сведения о номерах гостиницы
+class room {
+private:
+	int number; // номер комнаты
+	int price; // цена номера
+public:
+	void setRoom(int n, int p) {
+		if (n>0)
+			number = n;
+		else cout << "incorrect number";
+		if (p>0) 
+			price = p;
+		else cout << "incorrect price";
+	}
+};
+
+// бронь
+class reservation {
+private:
+	person quest; // сведения о постояльце
+	room residence; // сведения о занятом им номере
+	date arrival; // дата заселения
+	date exit; // дата выезда из гостиницы
+public:
+	void setReservation(person q, room r, date d1, date d2) {
+		quest = q;
+		residence = r;
+		arrival = d1;
+		exit = d2;
+	}
+
+};
+
+// VIP персона
+class VIP_person : public person {
+private:
+	int VIP_status = 1; // уровень статуса
+public:
+	void setStatus(int s) {
+		VIP_status = s;
+	}
+	void printStatus() {
+		cout << "VIP status = " << VIP_status << endl;
+	}
+};
+
+template <class T>
+//работник, также относится к персоне
+class employee : public person {
+private:
+	date start_work = date(1, 1, 2001); // дата начала работы
+	string post; // занятый пост
+	T salary; // зарплата // значение любого типа
+public:
+	employee(date s, string p, T sal) {
+		start_work = s;
+		post = p;
+		salary = sal;
+	}
+
+	void print() {
+		//cout << start_work << endl;
+		cout << FIO << " ";
+		birth.print();
+		cout << "Start work: ";
+		start_work.print();
+		cout << post << ", " << "salary: " << salary << "\n";
+	}
+};
+
+int main() {
+	ord_list<int> list1;
+	list1.add(2);
+     
+	ord_list<int>::Iterator list_iter;
+	list_iter = list1.begin();
+	cout << *list_iter << endl;
+	list1.add(1);
+	--list_iter;
+	cout << *list_iter << endl;
+	list1.add(1);
+	list1.add(0);
+	list1.add(9);
+	list1.add(10);
+	list_iter = list1.find(9);
+	cout << *list_iter << endl;
+    auto iter2 = list1.begin();
+	cout << *iter2 << endl;
+
+	for (list_iter = list1.begin(); list_iter != list1.end(); ++list_iter)
+	{
+		cout << *list_iter << " ";
+	}
+	cout << std::endl;
+
+
+
+
+/*
+	date a(3, 1, 2000);
+	date b(a);
+	a.print(); 
+	b.print();
+	a = b;
+	cout << "-------------\n";
+	a.print();
+	b.print();
+	cout << "--------------------------\n";
+	VIP_person Ann;
+	cout << "VIP person: \n";
+	Ann.setPerson("Madison Ann Rat", a);
+	Ann.print();
+	Ann.printStatus();
+	cout << "----------------------------------\n";
+	cout << "emlpoyee1: \n";
+	employee<string> Ivan(a, "Cleaner", "food");
+	Ivan.setPerson("Ivanov Ivan Ivanovich", a);
+	Ivan.print();
+	cout << "----------------------------------\n";
+	cout << "emlpoyee2: \n";
+	employee<double> Anton(a, "Manager", 50000.5);
+	Anton.setPerson("Anton Antonov Antonovich", a);
+	Anton.print();
+*/
+
+	system("pause");
+}
---
+++ list.h
@@ -0,0 +1,199 @@
+#pragma once
+#include <stddef.h>
+
+// двусв€зный упор€доченный список
+//
+//
+
+// структура двусв€зного списка
+template <class T>
+struct list {
+	list* next, *last;
+	T data;
+};
+
+// класс упор€доченного списка с итератором
+template <class temp>
+class ord_list
+{
+	friend class Iterator;
+protected:
+	int size = 0;
+	list<temp>* h, * t;
+
+public:
+
+
+	class Iterator {
+		friend class ord_list<temp>;
+		list<temp>* ptr;
+	public:
+		Iterator() {
+			ptr = NULL;
+		}
+		Iterator(list<temp>* l) {
+			ptr = l;
+		}
+
+		Iterator& operator=(const Iterator& it)
+		{
+			ptr = it.ptr;
+			return *this;
+		}
+		Iterator& operator++() {
+			if (ptr->next != NULL)
+				ptr = ptr->next;
+			return *this;
+		}
+		Iterator& operator--() {
+			if (ptr->last != NULL)
+				ptr = ptr->last;
+			return *this;
+		}
+		temp& operator*() { return ptr->data; }
+		bool operator == (const Iterator& it) 
+		{
+			return (ptr == it.ptr);
+		}
+		bool operator!=(const Iterator& it) 
+		{
+			return !(it.ptr == ptr);
+		}
+
+	};
+
+	Iterator begin() { return Iterator(h); }
+	Iterator end() { return Iterator(t); }
+		Iterator find(temp a) {
+			auto cur = h;
+			while ((cur->data!=a)&&(cur->next != NULL)) {
+				cur = cur->next;
+		}
+			return Iterator(cur);
+		}
+
+
+public:
+	ord_list() {
+		h = NULL;
+		t = NULL;
+
+	}
+	~ord_list() {
+		list<temp>* cur = new list<temp>;
+		while (h != NULL) {
+			cur = h;
+			h = h->next;
+			delete(cur);
+		}
+	}
+
+	// добавление элемента в соответсвующем пор€дке
+	void add(temp a) {
+		list<temp>* cur = new list<temp>;
+		cur->data = a;
+		if (h == NULL) {
+			cur->next = NULL;
+			cur->last = NULL;
+			h = cur; t = cur;
+		}
+		else {
+			list<temp>* cur1 = new list<temp>;
+			cur1 = h;
+
+			while ((cur1->next != NULL) && (cur->data > cur1->data)) {
+				cur1 = cur1->next;
+			}
+
+			if ((cur1->next == NULL) && (cur->data > cur1->data)) {
+				cur->next = NULL;
+				cur->last = cur1;
+				cur->last->next = cur;
+				t = cur;
+				cur = h;
+			}
+			else {
+				if (cur1->last == NULL) {
+					cur->last = NULL;
+					cur->next = h;
+					h->last = cur;
+					//cur = h;
+				}
+				else {
+					cur->next = cur1;
+					cur->last = cur1->last;
+					cur->last->next = cur;
+					cur->next->last = cur;
+					cur = h;
+				}
+			}
+			h = cur;
+
+		}
+		size++;
+
+	}
+
+	// изъ€ть элемент с начала
+	void push_front() {
+		list<temp>* cur = new list<temp>;
+		cur = h;
+		if (h != NULL) {
+			if ((cur->next == NULL) && (cur->last == NULL)) {
+				delete(h);
+				h = NULL;
+				t = NULL;
+				//cur = h;
+			}
+			else {
+				cur->last->next = NULL;
+				delete cur;
+				cur = h;
+				h = cur;
+			}
+			size--;
+		}
+
+	}
+
+	// изъ€ть элемент с конца
+	void push_back() {
+		list<temp>* cur = new list<temp>;
+		cur = t;
+		if (t != NULL) {
+			if ((cur->next == NULL) && (cur->last == NULL)) {
+				delete(t);
+				h = NULL;
+				t = NULL;
+				//cur = h;
+			}
+			else {
+				t = t->next;
+				t->last = NULL;
+				delete(cur);
+				cur = NULL;
+			}
+			size--;
+		}
+
+	}
+
+	// очистить список
+	void clear() {
+		list<temp>* cur = new list<temp>;
+		while (h != NULL) {
+			cur = h;
+			h = h->next;
+			delete(cur);
+		}
+		h = NULL;
+		t = NULL;
+		size = 0;
+	}
+	int get_size() {
+		return size;
+	}
+};
+
+
+
